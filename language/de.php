3345a759d50dae34ab1b9c819d57b07b7c262c63
Melvin Lauber <molvinlauber@gmail.com>
2020-01-25 17:48:03 +0100

    friends
    - add navigation friends
    - todo: david friends page & user read out

--- language/de.php
+++ language/de.php
@@ -11,6 +11,7 @@ define('DISCOVER', 'Entdecken');
 define('MY_SONGS', 'Meine Songs');
 define('ARTISTS', 'Künstler');
 define('EVENTS', 'Konzerte');
+define('FRIENDS', 'Freunde');
 define('SETTINGS', 'Einstellungen');
 define('MY_PLAYLISTS', 'Meine Playlists');
 
